3090ddfa51db2ea26f60694f5ff49990ca5b4ee4
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-04-23 22:21:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot 
+  git clone https://github.com/ROYAL-JATT/HK-Productions-auto-filter-bot.git /HK-Productions-auto-filter-bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMoviesProviderBot
+  git clone $UPSTREAM_REPO /HK-Productions-auto-filter-bot
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /HK-Productions-auto-filter-bot 
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting HK-Productions-auto-filter-bot...."
 python3 bot.py
